d731fc5ed00056351cd622c3ebc3315a18464396
LancerShine <rharmed819@126.com>
2017-10-16 16:19:52 -0700

    rharmed

---
+++ README.md
@@ -0,0 +1,12 @@
+# steal-react-example
+
+1. Clone this repo.
+2. Run `npm install`
+3. Start an `http-server` in the root directory.
+4. Open in your browser.
+
+You can click on the “Welcome to…” title to see the `onClick` event fire.
+
+This example uses [steal-jsx](https://www.npmjs.com/package/steal-jsx).
+
+![Steal-React Example Screenshot](screenshot.png?raw=true "Simple Steal-React Example App")
---
+++ index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Title from './title.jsx';
+
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Title greeting={this.props.greeting} />
+        <ul>
+          <li>
+            To learn more about StealJS, visit <a href="http://stealjs.com/" target="_blank">
+              StealJS.com
+            </a>
+          </li>
+          <li>
+            For live help with simple questions, check out <a href="https://gitter.im/stealjs/steal" target="_blank">
+              the Gitter chat
+            </a>
+          </li>
+          <li>
+            For more complex questions, post to <a href="http://forums.donejs.com/c/stealjs" target="_blank">
+              the forum
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(<HelloMessage greeting="Welcome to your first StealJS and React app!" />, document.getElementById('app'));
